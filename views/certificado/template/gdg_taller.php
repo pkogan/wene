13f47a47644ac2c8b9a991865b037daebc1762fd
pkogan <pkogan@debian-fai.fi.uncoma.edu.ar>
2020-11-17 12:07:50 -0300

    template gdg

--- views/certificado/template/gdg_taller.php
+++ views/certificado/template/gdg_taller.php
@@ -34,7 +34,7 @@ use Da\QrCode\QrCode;
         <h3>Se certifica que <b><?= mb_strtoupper($model->idPersona0->apellidoNombre); ?></b>, DNI Nº <b><?= number_format($model->idPersona0->dni,0,',','.') ?></b>
             <b><?= strtolower($model->idLote0->idTipoCertificado0->tipo) ?></b> <?=$model->idLote0->getConector() ?> <?= $model->idLote0->idActividad0->idTipoActividad0->tipo ?>
 <!--            ha presentado el artículo-->
-            <b><i>"<?=$model->observacion?>"</i></b> en el marco del </b>
+            <b><i><?=$model->observacion?>"</i></b> en el marco del </b>
             <b>"<?= $model->idLote0->idActividad0->nombre ?>"</b>. 
             <?= ($model->idLote0->idActividad0->norma!='')?'Avalado por '.$model->idLote0->idActividad0->norma.'. ':''?>
             <?= $model->idLote0->observacion ?></h3>

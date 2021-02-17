262c89d4a0bbb3df2c2a54bb6abccfe815eccefc
pkogan <pkogan@debian-fai.fi.uncoma.edu.ar>
2021-02-17 10:45:30 -0300

    index site registro histórico y convenios

--- views/site/index.php
+++ views/site/index.php
@@ -37,6 +37,16 @@ $this->title = 'wene - Certificados Facultad de Informática';
                 <p>Con el objetivo de agilizar los procesos, hacer un aporte al medio ambiente y ahorrar papel, todas las actividades realizadas por la Facultad de Informática tendrán solamente certificados digitales</p>
                 <p> <a class="btn btn-default" href="<?= \yii\helpers\Url::base('https') ?>/img/ResCD-031-Ratificar-ResAdRef077-Certificados-Digitales-EXTescopia.pdf">ResCD Nro 031/20 &raquo;</a>
                     <a class="btn btn-default" href="<?= \yii\helpers\Url::base('https') ?>/img/ResCD-064-Ratificar-ResAdRef117-Certificados-Digitales-Academica-SAescopia.pdf">ResCD Nro 064/20 &raquo;</a>
+                  
+                </p>
+                
+                
+                <h2>Registro histórico de Actividades</h2>
+
+                <p> Lxs titulares que han recibido certificados por mail, puenden acceder a su historial de actividades/certificados emitidos por las Organizaciones que utilizan el sistema</p>
+                <p> <?= yii\helpers\Html::a('Buscar mis Certificados &raquo;', ['/certificado/mis'], ['class' => 'btn btn-default']) ?>
+                    
+                    
                 </p>
     <!--                <p><a class="btn btn-default" href="">Resolución XXX/20 &raquo;</a></p>-->
             </div>
@@ -56,6 +66,9 @@ $this->title = 'wene - Certificados Facultad de Informática';
                 <p>RESOLUCIÓN N°1135 / EXPEDIENTE N°8120-003418/2019 12/9/2019</p>
 
                 <p><a class="btn btn-default" href="<?= \yii\helpers\Url::base('http') ?>/img/r_1135_19.pdf">Resolución CPE 1135/19 &raquo;</a></p>
+                <h2>Convenios con Facultad de Humanidades y aten</h2>
+                <p>Se ha realizado convenio para emisión de certificados con Secretaría de extensión de la Facultad de Humanidades <a class="btn btn-default" href="<?= \yii\helpers\Url::base('https') ?>/img/0076_20_AvalCertificadoVirtualExtension.pdf">ResCD FH Nro 076/20 &raquo;</a>
+                   y con la Asociación de Trabajadores de Educación de Neuquen (aten) <a class="btn btn-default" href="<?= \yii\helpers\Url::base('https') ?>/img/convenioatenwene.pdf">Convenio aten-FAIF &raquo;</a></p>
             </div>
         </div>
 

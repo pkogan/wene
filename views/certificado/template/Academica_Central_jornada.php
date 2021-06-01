12837bc6c7406f6cabc478ef22764b0ec1d84166
pkogan <pkogan@debian-fai.fi.uncoma.edu.ar>
2021-06-01 12:57:14 -0300

    template Académica

--- views/certificado/template/Academica_Central_jornada.php
+++ views/certificado/template/Academica_Central_jornada.php
@@ -31,7 +31,7 @@ use Da\QrCode\QrCode;
     <h3>Se certifica que <b><?= mb_strtoupper($model->idPersona0->apellidoNombre,'UTF-8'); ?></b>, DNI Nº <b><?= number_format($model->idPersona0->dni,0,',','.'); ?></b><br/>
         <b><?= $model->idLote0->idTipoCertificado0->tipo ?></b> al <b><?= $model->idLote0->idActividad0->idTipoActividad0->tipo ?></b>
         
-        <b><?= $model->idLote0->idActividad0->nombre ?></b><!--, el <?= $model->idLote0->idActividad0->getFechaTexto(); ?>.--><br>
+        <b><?= $model->idLote0->idActividad0->nombre ?></b><!--, el <?= $model->idLote0->idActividad0->getFechaTexto(); ?>.-->.
         <!--y con una duración de <?= $model->idLote0->idActividad0->duracion ?> <?= $model->idLote0->idActividad0->medidaDuracion ?>.-->
         <?= ($model->idLote0->idActividad0->norma!='')?'Avalado por '.$model->idLote0->idActividad0->norma.'. ':''?>
         <?= $model->idLote0->observacion ?></h3>

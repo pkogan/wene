a62316a12502930df9c5a4fbee79cd76393e0ed1
pkogan <pkogan@debian-fai.fi.uncoma.edu.ar>
2021-06-02 13:39:24 -0300

    template Académica

--- views/certificado/template/Academica_Central_jornada.php
+++ views/certificado/template/Academica_Central_jornada.php
@@ -39,26 +39,26 @@ use Da\QrCode\QrCode;
     <h3 >Neuquén, <?= $model->idLote0->getFechaTexto(); ?>.</h3>
 
            <div class="row" style="font-size: 11px;">
-            <div class="col-xs-3" style="background-image: url('img/firmas/demo.png'); background-repeat: no-repeat; background-size: 17% auto; background-position: top">
+            <div class="col-xs-3" style="background-image: url('img/firmas/firmalidia.png'); background-repeat: no-repeat; background-size: 35% auto; background-position: top">
                 <br/>
                 <br/>
                 <br/>
-                <b>Mg. Lidia Lopez</b><br/>
+                <b>Mg. Lidia Marina López</b><br/>
                 Secretaría Académica<br/>
                 Universidad Nacional del Comahue
             </div>
 
-            <div class="col-xs-4" style="background-image: url('img/firmas/demo.png'); background-repeat: no-repeat; background-size: 20% auto; background-position: top">
+            <div class="col-xs-4" style="background-image: url('img/firmas/firmacarina.png'); background-repeat: no-repeat; background-size: 20% auto; background-position: top">
                 <br/>
                 <br/>
                 <br/>
-                <b>Dra. Carina Fracchia</b><br/>
-                Co-Directora Maestría en Enseñanza en<br/>
-                Escenarios Digitales<br/>
+                    <b>Dra. Carina Fracchia</b><br/>
+                Cordinadora de la Maestría<br/>
+                Enseñanza en Escenarios Digitales<br/>
                 Facultad de Informática - UNComahue
             </div>
 
-            <div class="col-xs-3" style="background-image: url('img/firmas/firmapk.png'); background-repeat: no-repeat; background-size: 20% auto; background-position: top">
+            <div class="col-xs-3" style="background-image: url('img/firmas/firmapk.png'); background-repeat: no-repeat; background-size: 25% auto; background-position: top">
                 <br/>
                 <br/>
                 <br/>

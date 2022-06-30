8d8014a215d6db15addfe3285c0bdd4d621ba2ef
pkogan <pkogan@debian-fai.fi.uncoma.edu.ar>
2022-06-30 06:21:14 -0300

    template curza posgrado

---
+++ views/certificado/template/curza_basicoV.php
@@ -0,0 +1,64 @@
+<?php
+/* @var $model \app\models\Certificado */
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+if(!is_null($model->idLote0->idActividad0->linkNorma)&&$model->idLote0->idActividad0->linkNorma!=''){
+    $norma= Html::a($model->idLote0->idActividad0->norma,['norma', 'hash' => $model->hash]);
+}else{
+    $norma=$model->idLote0->idActividad0->norma;
+}?>
+<div style="text-align: center">
+    <br>
+    <br>
+    <div class="row" >
+
+        <div class="col-xs-12">
+            <img  width="20%" src="img/curza.png" alt="UNCo"/></div>
+<!--        <img class="logo" src="img/sadosky.png" alt="Sadosky"/>-->
+<!--
+        <div class="col-xs-5">
+            <img  width="65%" src="img/faif.png" alt="Facultad de Informática"/></div>-->
+    </div>
+
+    <h1 >Certificado Digital</h1>
+<?php echo DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            //'idCertificado',
+            'idPersona0.apellidoNombre',
+            'idPersona0.dni',
+            ['attribute'=>'idPersona0.legajo',
+            'visible'=>$model->idPersona0->legajo!=''],
+            ['label'=>'Condición Certificada','value'=>$model->idLote0->idTipoCertificado0->tipo],
+            ['label'=>'Tipo Actividad','value'=>$model->idLote0->idActividad0->idTipoActividad0->tipo],
+            ['label'=>'Nombre Actividad','value'=>$model->idLote0->idActividad0->nombre,
+                'visible'=>$model->idLote0->idActividad0->nombre!=$model->idLote0->idTipoCertificado0->tipo],
+            ['label'=>'Descripción Actividad','value'=>$model->idLote0->observacion,
+                'visible'=>$model->idLote0->observacion!=''],
+            ['label'=>'Nota','value'=>$model->observacion,
+                'visible'=>$model->observacion!=''],
+            ['label'=>'Dependencia Otorgante','value'=>$model->idLote0->idActividad0->idDependencia0->nombre],
+            [//'attribute'=>'idLote0.idActividad0.norma',
+             'label'=>'Norma','value'=>$norma, 'format'=>'raw',  
+            'visible'=>$model->idLote0->idActividad0->norma!=''],
+            ['label'=>'Fecha','value'=>$model->idLote0->idActividad0->getFechaTexto(),
+                'visible'=>$model->idLote0->idTemplate!= \app\models\Template::ADJUNTO],
+            ['label'=>'Duración','value'=>$model->idLote0->idActividad0->duracion.' '.$model->idLote0->idActividad0->medidaDuracion,
+            'visible'=>$model->idLote0->idActividad0->duracion!=''],
+            //'idLote0.observacion',
+            
+            ['label'=>'Validar','value'=>'Accediendo al siguiente código QR se puede validar el certificado. '.
+                'Si no tienen lector de códigos QR el certificado se puede validar entrando a '.\yii\helpers\Url::base('https').' con el código '.$model->hash],
+                 //'<a href="'.\yii\helpers\Url::base('https').'">'.\yii\helpers\Url::base('https').'</a> con el código <b>'.$model->hash.'</b>'],
+            ['label' => 'QR', 'value' => $qrCode->writeDataUri(), 'format' => ['image', []]],
+            //'idEstado0.estado',
+            ['label'=>'Código del Certificado','value'=>$model->hash],
+            ['label'=>'Fecha Emisión','value'=>$model->idLote0->getFechaTexto()],
+            ['label'=>'Estado Certificado','value'=>$model->idEstado0->estado]
+            
+            
+        ],
+    ]); 
+    ?>
+
+</div>

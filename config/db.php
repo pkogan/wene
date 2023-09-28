cade21fc709f9d45fe6dbdfb08420787bc63c465
Julian Murphy <njmdistrisoft@gmail.com>
2023-09-28 16:10:57 -0300

    use env vars

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
+$HOST = $_ENV['MYSQL_HOST'];
+$DB_NAME = $_ENV['MYSQL_DATABASE'];
+$DB_USER = $_ENV['MYSQL_USER'];
+$DB_PASS = $_ENV['MYSQL_PASSWORD'];
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=wene',
-    'username' => 'root',
-    'password' => 'test',
+    'dsn' => 'mysql:host=' . $HOST . ';dbname=' . $DB_NAME,
+    'username' => $DB_USER,
+    'password' => $DB_PASS,
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];

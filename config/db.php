ad5650bcb81346a46a98a62bac0d4f6860b00f80
pkogan <pablo.kogan@gmail.com>
2023-11-16 14:21:17 -0300

    templatebalotajeCNE

--- config/db.php
+++ config/db.php
@@ -2,7 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=wene',
+ //   'dsn' => 'mysql:host=db;dbname=wene',
+ 'dsn' => 'mysql:host=db;dbname=testcurza',
     'username' => 'root',
     'password' => 'test',
     'charset' => 'utf8',

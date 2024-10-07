ecaf0c908eb1dee3d347e964704ca4d90b7d5dfc
pkogan <pablo.kogan@gmail.com>
2024-10-07 10:24:29 -0300

    FALE DNI EXTERIOR

--- views/certificado/template/FALE_jornada_2.php
+++ views/certificado/template/FALE_jornada_2.php
@@ -30,7 +30,7 @@ use Da\QrCode\QrCode;
 
 
 
-    <h3>Se certifica que <b><?= mb_strtoupper($model->idPersona0->apellidoNombre,'UTF-8'); ?></b>, DNI Nº <b><?= number_format($model->idPersona0->dni,0,',','.'); ?></b><br/>
+    <h3>Se certifica que <b><?= mb_strtoupper($model->idPersona0->apellidoNombre,'UTF-8'); ?></b>, DNI Nº <b> <?=$model->idPersona0->getDnioIdExt()?> </b><br/>
         <b><?= $model->idLote0->idTipoCertificado0->tipo ?></b> <?=$model->idLote0->getConector() ?> <b><?= $model->idLote0->idActividad0->idTipoActividad0->tipo ?></b>
         
         <b><?= $model->idLote0->idActividad0->nombre?></b>
